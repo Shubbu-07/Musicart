e66ab3e9dead734f82e3a1ecf4cc81d10e1a8a60
Shubbu-07 <95517382+Shubbu-07@users.noreply.github.com>
2024-07-19 14:54:22 +0530

    Update img names and Home.jsx

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import bannerImg from "../Assets/image_5-removebg-preview 1.png";
+import bannerImg from "../Assets/banner-image.png";
 import gridViewIcon from "../Assets/grid-view-icon.svg";
 import listViewIcon from "../Assets/list-view-icon.svg";
 
@@ -10,7 +10,7 @@ const Home = () => {
       <Navbar />
 
       <section className="banner">
-      <div className="banner-content">
+        <div className="banner-content">
           <h1>Grab upto 50% off on Selected headphones</h1>
         </div>
         <div className="banner-img">
@@ -21,28 +21,32 @@ const Home = () => {
       <section className="search">
         <input type="text" placeholder="Search by Product Name" />
       </section>
-      
+
       <section className="filters">
         <div className="d-flex">
-          <button><img src={gridViewIcon} alt="" /></button>
-          <button><img src={listViewIcon} alt="" /></button>
+          <button>
+            <img src={gridViewIcon} alt="" />
+          </button>
+          <button>
+            <img src={listViewIcon} alt="" />
+          </button>
         </div>
         <div className="filter-content">
-        <select>
-                        <option>Headphone type</option>
-                    </select>
-                    <select>
-                        <option>Company</option>
-                    </select>
-                    <select>
-                        <option>Colour</option>
-                    </select>
-                    <select>
-                        <option>Price</option>
-                    </select>
-                    <select>
-                        <option>Sort by : Featured</option>
-                    </select>
+          <select>
+            <option>Headphone type</option>
+          </select>
+          <select>
+            <option>Company</option>
+          </select>
+          <select>
+            <option>Colour</option>
+          </select>
+          <select>
+            <option>Price</option>
+          </select>
+          <select>
+            <option>Sort by : Featured</option>
+          </select>
         </div>
       </section>
     </div>
