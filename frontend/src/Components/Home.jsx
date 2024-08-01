0a5982dd45efd30e210e5cffebb3317de06559cd
Shubbu-07 <95517382+Shubbu-07@users.noreply.github.com>
2024-08-01 19:09:31 +0530

    Removed Search bar & Filters

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
 import bannerImg from "../Assets/banner-image.png";
-import gridViewIcon from "../Assets/grid-view-icon.svg";
-import listViewIcon from "../Assets/list-view-icon.svg";
 
 const Home = () => {
   return (
@@ -15,38 +13,6 @@ const Home = () => {
         </div>
         <div className="banner-img">
           <img src={bannerImg} alt="Banner-Image" />
-        </div>
-      </section>
-
-      <section className="search">
-        <input type="text" placeholder="Search by Product Name" />
-      </section>
-
-      <section className="filters">
-        <div className="d-flex">
-          <button>
-            <img src={gridViewIcon} alt="" />
-          </button>
-          <button>
-            <img src={listViewIcon} alt="" />
-          </button>
-        </div>
-        <div className="filter-content">
-          <select>
-            <option>Headphone type</option>
-          </select>
-          <select>
-            <option>Company</option>
-          </select>
-          <select>
-            <option>Colour</option>
-          </select>
-          <select>
-            <option>Price</option>
-          </select>
-          <select>
-            <option>Sort by : Featured</option>
-          </select>
         </div>
       </section>
     </div>
